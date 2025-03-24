927b31f763b8e25cced8037dbd7f5fb3449819ee
Gustavo Sanchez <gustavosanchezgalarza@gmail.com>
2025-03-23 19:24:40 -0500

    Add google fonts and hero

---
+++ src/components/hero.tsx
@@ -0,0 +1,23 @@
+export const Hero = () => {
+  return (
+    <section>
+      <h2 className="font-[family-name:var(--font-outfit)] text-5xl font-extrabold">
+        I&apos;m a fullstack developer
+      </h2>
+      <p className="font-[family-name:var(--font-ibm-plex-mono)] text-xl">
+        I&apos;m engineer in electronics and telecommunications graduated from
+        the University of Cauca in Colombia, with +25 years of experience in
+        technical, pre-sales and sales areas.
+        <br />
+        During the pandemic I wanted totake advantage of my free time and began
+        to learn about the technologies on which the WWW is built today. For the
+        last 5 years I&apos;ve been learning the fundamentals of HTML, CSS and
+        Javascript. <br />
+        Also backend technologies like node.js, creating andconsuming APIs with
+        JS and exploring some tools like Prisma.io (ORM), development of
+        applications in the cloud and databases such as MongoDB and Postgresql
+        for data storage required in the development of full-stack applications.
+      </p>
+    </section>
+  );
+};
